459867f9a7c8a49a5535451081892a4019521e52
sana <sana@NakamuranoMacBook-Pro.local>
2021-06-11 14:30:19 +0900

    learn jsx  Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 
 function App() {
   return (
-    <div ><h1>Welcome to React</h1></div>
+    <>
+    <input type ="text" onChange={() => {console.log('clicked')}}/>
+    <h1>Welcome to React</h1>
+    </>
   );
 }
 
