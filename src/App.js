406c9db03e433a387cc9c64e02c1bd0e324de3bb
sana <sana@NakamuranoMacBook-Pro.local>
2021-06-11 21:21:32 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 const App = () => {
+  const progiles = [
+    {name:"Taro",age:10},
+    {name:"Hanako"}
+  ]
   return (
     <>
-    <input type ="text" onChange={() => {console.log('clicked')}}/>
-    <h1>Welcome to React</h1>
+    {
+      progiles.map((progiles,index) => {
+        return <User name = {progiles.name} age = {progiles.age} key = {index}/>
+      })
+    }
+
+    
     </>
   );
 }
+const User = (props) =>{
+  return <div>Hi Im {props.name},{props.age}</div>
+}
 
+//デフォルトの値を設定できる
+User.defaultProps = {
+  age:1
+}
 export default App;
